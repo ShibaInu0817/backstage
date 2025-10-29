7d13c5700ddf9e8da9eaa9ac03bbac5c2bf306e0
Tee Tian Xiang <learningtocode0817@gmail.com>
2025-10-29 17:45:05 +0800

    Reimplement cqrs into same api gateway

--- apps/api-gateway/src/app/messages/create/module.ts
+++ apps/api-gateway/src/app/messages/create/module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateMessageController } from './controller';
 import { MessageInfraModule } from '@boilerplate/messages-infra';
+import { CreateMessageHandler } from './handler';
 
 @Module({
   imports: [CqrsModule, MessageInfraModule],
+  providers: [CreateMessageHandler],
   controllers: [CreateMessageController],
 })
 export class CreateMessageModule {}
