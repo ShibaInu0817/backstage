2c7cc4fb2c401bba0d134a44ddf25caf2d95ff48
Tee Tian Xiang <learningtocode0817@gmail.com>
2025-10-29 11:15:47 +0800

    Add message-command-processor application with initial setup
    - Created the message-command-processor application with necessary configurations.
    - Implemented core modules including message command handler and error handling.
    - Added environment configuration files and example files for local development.
    - Updated .gitignore to include .env files and other build artifacts.
    - Enhanced nx.json to exclude the new application from specific targets.
    - Updated README with instructions for generating new applications.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Please refer to the [todo.md](todo.md) file for the tasks to be done.
 
+
+
+When generating a new application
+- add tags to the application
+- remove rootDir from tsconfig.app.json
+- nx reset && nx sync
+- modified main.ts
--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -2,14 +2,8 @@ import { Module } from '@nestjs/common';
 import { MessageModule } from './messages/module';
 import { HttpModule } from '@boilerplate/http';
 import { MonolithMessageModule } from './monolith-messages/module';
-import { MessageCommandHandlerModule } from './message-command-handler/module';
 
 @Module({
-  imports: [
-    HttpModule,
-    MessageModule,
-    MonolithMessageModule,
-    MessageCommandHandlerModule,
-  ],
+  imports: [HttpModule, MessageModule, MonolithMessageModule],
 })
 export class AppModule {}
