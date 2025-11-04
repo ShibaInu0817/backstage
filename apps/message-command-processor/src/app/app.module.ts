ca13fa99b6b5ef893036660bb8bfd60b0c13ccc9
Tee Tian Xiang <learningtocode0817@gmail.com>
2025-11-04 17:23:18 +0800

    Remove @boilerplate/message-command-processor

--- apps/message-command-processor/src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@boilerplate/http';
-import { MessageCommandHandlerModule } from './message-command-handler/module';
-
-@Module({
-  imports: [HttpModule, MessageCommandHandlerModule],
-})
-export class AppModule {}
--- apps/message-command-processor/src/app/message-command-handler/module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CreateMessageCommandHandlerModule } from './create/module';
-
-@Module({
-  imports: [CreateMessageCommandHandlerModule],
-})
-export class MessageCommandHandlerModule {}
